b3281fd46591062348b04467314daede0bd7d76a
Tomas <tomasnyiri@gmail.com>
2022-03-02 21:14:16 +0100

    Pridanie testovacieho programu pre zobrazenie hran vo videu a detekcia cervenej farby

---
+++ edges.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+print(cv2.__version__)
+
+
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    _, frame = vid.read()
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    edges = cv2.Canny(frame,100,100)
+  
+    #filters
+    lower_red = np.array([150,150,90])
+    upper_red = np.array([180,255,150])
+
+    mask = cv2.inRange(hsv,lower_red,upper_red)
+    res =cv2.bitwise_and(frame,frame, mask=mask)
+
+    # Display the resulting frame
+    cv2.imshow('The_Input_Frame',frame)
+    cv2.imshow('edges', edges)
+    cv2.imshow('Mask', mask)
+    cv2.imshow('Res', res)
+      
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+
+vid.release()
+cv2.destroyAllWindows()
